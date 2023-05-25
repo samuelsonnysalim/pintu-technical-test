2b1b876679fc23443bcfe04f317d4081ac47f508
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-25 22:45:21 +0700

    Move menu to component folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import classNames from 'classnames';
 import { Inter } from 'next/font/google';
-import Menu from './menu';
+import Menu from './component/menu';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.cdnfonts.com/css/neue-haas-grotesk-display-pro"
+          rel="stylesheet"
+        />
+      </head>
       <body className={classNames(inter.className, 'bg-white')}>
         <div className="w-page m-auto">
-          <header className="py-8 px-6">
+          <header className="py-8 px-6 mb-4">
             <a href="/" className="block w-[75px] h-4 bg-logo float-left" />
             <Menu />
           </header>
