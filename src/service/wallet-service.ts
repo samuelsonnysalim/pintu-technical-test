ff41b25f030f1d9a28653faff9cd19672a6a9a35
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-26 11:32:52 +0700

    Create WalletService

---
+++ src/service/wallet-service.ts
@@ -0,0 +1,39 @@
+import { createService } from './core';
+
+interface Wallet {
+  currencyGroup: string;
+  tokenSymbol: string;
+  decimal_point: number;
+  tokenType: string;
+  blockchain: string;
+  explorer: string;
+  listingDate: string;
+  blockchainName: string;
+  logo: string;
+}
+
+interface SupportedCurrency {
+  currencyGroup: string;
+  color: string;
+  currencySymbol: string;
+  name: string;
+  logo: string;
+  decimal_point: number;
+  listingDate: string;
+  wallets: Wallet[];
+}
+
+interface SupportedCurrenciesResponse {
+  code: string;
+  message: string;
+  payload: SupportedCurrency[];
+}
+
+const WalletService = {
+  listSupportedCurrencies: createService<SupportedCurrenciesResponse>({
+    url: 'wallet/supportedCurrencies',
+    method: 'get',
+  }),
+};
+
+export default WalletService;
