821c77e44a1e9d9abd6813bff8dce006426a9c02
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-26 21:34:35 +0700

    Refactor unit test

--- src/app/component/__tests__/search-currency.test.tsx
+++ src/app/component/__tests__/search-currency.test.tsx
@@ -4,75 +4,77 @@ import ClientProvider from '@pintu/technical-test/app/client-provider';
 import SearchCurrency from '@pintu/technical-test/app/component/search-currency';
 import WalletService from '@pintu/technical-test/service/wallet-service';
 
-jest.mock('@pintu/technical-test/service/wallet-service', () => ({
-  listSupportedCurrencies: jest.fn().mockResolvedValue({
-    code: 'success',
-    message: '',
-    payload: [
-      {
-        currencyGroup: 'IDR',
-        color: '#0A68F4',
-        currencySymbol: 'Rp',
-        name: 'Rupiah Token',
-        logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_IDRT.svg',
-        decimal_point: 0,
-        listingDate: '2020-09-15T09:43:42Z',
-        wallets: [],
-      },
-      {
-        currencyGroup: 'BTC',
-        color: '#F78B1A',
-        currencySymbol: 'BTC',
-        name: 'Bitcoin',
-        logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_BTC.svg',
-        decimal_point: 8,
-        listingDate: '2020-09-15T09:43:45Z',
-        wallets: [
-          {
-            currencyGroup: 'BTC',
-            tokenSymbol: 'BTC',
-            decimal_point: 8,
-            tokenType: 'Bitcoin',
-            blockchain: 'Bitcoin',
-            explorer: 'https://explorer.bitcoin.com/btc/tx/',
-            listingDate: '2020-09-15T09:43:45Z',
-            blockchainName: 'Bitcoin',
-            logo: 'https://s3.ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/blockchain/Bitcoin.svg',
-          },
-        ],
-      },
-      {
-        currencyGroup: 'ETH',
-        color: '#9011FE',
-        currencySymbol: 'ETH',
-        name: 'Ethereum',
-        logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
-        decimal_point: 18,
-        listingDate: '2020-09-15T09:43:46Z',
-        wallets: [
-          {
-            currencyGroup: 'ETH',
-            tokenSymbol: 'ETH',
-            decimal_point: 18,
-            tokenType: 'ERC-20',
-            blockchain: 'Ethereum',
-            explorer: 'https://etherscan.io/tx/',
-            listingDate: '2020-09-15T09:43:46Z',
-            blockchainName: 'Ethereum',
-            logo: 'https://s3.ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/blockchain/ERC-20.svg',
-          },
-        ],
-      },
-    ],
-  }),
-}));
+jest.mock('@pintu/technical-test/service/wallet-service');
 
 const listSupportedCurrencies =
-  WalletService.listSupportedCurrencies as jest.Mocked<
+  WalletService.listSupportedCurrencies as jest.MockedFunction<
     typeof WalletService.listSupportedCurrencies
   >;
 
 describe('SearchCurrency', () => {
+  beforeEach(() => {
+    listSupportedCurrencies.mockResolvedValue({
+      code: 'success',
+      message: '',
+      payload: [
+        {
+          currencyGroup: 'IDR',
+          color: '#0A68F4',
+          currencySymbol: 'Rp',
+          name: 'Rupiah Token',
+          logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_IDRT.svg',
+          decimal_point: 0,
+          listingDate: '2020-09-15T09:43:42Z',
+          wallets: [],
+        },
+        {
+          currencyGroup: 'BTC',
+          color: '#F78B1A',
+          currencySymbol: 'BTC',
+          name: 'Bitcoin',
+          logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_BTC.svg',
+          decimal_point: 8,
+          listingDate: '2020-09-15T09:43:45Z',
+          wallets: [
+            {
+              currencyGroup: 'BTC',
+              tokenSymbol: 'BTC',
+              decimal_point: 8,
+              tokenType: 'Bitcoin',
+              blockchain: 'Bitcoin',
+              explorer: 'https://explorer.bitcoin.com/btc/tx/',
+              listingDate: '2020-09-15T09:43:45Z',
+              blockchainName: 'Bitcoin',
+              logo: 'https://s3.ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/blockchain/Bitcoin.svg',
+            },
+          ],
+        },
+        {
+          currencyGroup: 'ETH',
+          color: '#9011FE',
+          currencySymbol: 'ETH',
+          name: 'Ethereum',
+          logo: 'https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
+          decimal_point: 18,
+          listingDate: '2020-09-15T09:43:46Z',
+          wallets: [
+            {
+              currencyGroup: 'ETH',
+              tokenSymbol: 'ETH',
+              decimal_point: 18,
+              tokenType: 'ERC-20',
+              blockchain: 'Ethereum',
+              explorer: 'https://etherscan.io/tx/',
+              listingDate: '2020-09-15T09:43:46Z',
+              blockchainName: 'Ethereum',
+              logo: 'https://s3.ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/blockchain/ERC-20.svg',
+            },
+          ],
+        },
+      ],
+    });
+  });
+
   it('should load component', () => {
     render(
       <ClientProvider>
