e5664001cd73608e52fc5e0ee7537f76c2b2922e
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-26 12:19:13 +0700

    Add margin-bottom on title panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SearchCurrency from './component/search-currency';
 export default function Home() {
   return (
     <>
-      <div className="flex">
+      <div className="flex mb-8">
         <h1 className="flex-none grow text-[28px] font-heading font-semibold">
           Harga Crypto dalam Rupiah Hari Ini
         </h1>
