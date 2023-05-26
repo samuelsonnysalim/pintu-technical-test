832ece18405b827c0486d7f9a94a695d1144e184
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-26 11:32:36 +0700

    Create createService function

---
+++ src/service/core.ts
@@ -0,0 +1,32 @@
+import axios, { Method } from 'axios';
+import { Path } from 'path-parser';
+
+axios.defaults.baseURL = `${location.href}api/`;
+
+interface ServiceOptions {
+  url: string;
+  method: Method | string;
+}
+
+interface RequestOptions<D> {
+  params: Record<string, any>;
+  body: D;
+  query: Record<string, string>;
+}
+
+type Service<T, D> = (options?: Partial<RequestOptions<D>>) => Promise<T>;
+
+export function createService<T = any, D = any>(
+  options: ServiceOptions,
+): Service<T, D> {
+  return async (request?: Partial<RequestOptions<D>>) =>
+    (
+      await axios.request<T>({
+        url: new Path(options.url).build(request?.params),
+        method: options.method,
+        data: request?.body,
+        params: request?.query,
+        ...(request || {}),
+      })
+    ).data;
+}
