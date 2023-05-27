548eab23df246719224daa7e8f4ed1c398d52c1b
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-27 12:26:55 +0700

    Add Breadcrumb component

---
+++ src/app/component/__tests__/breadcrumb.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import Breadcrumb from '@pintu/technical-test/app/component/breadcrumb';
+
+describe('Breadcrumb', () => {
+  it('should load component', () => {
+    render(<Breadcrumb />);
+
+    expect(screen.queryAllByText('>').length).toEqual(0);
+  });
+
+  it('should render breadcrumbs based on paths passed to the props', () => {
+    render(
+      <Breadcrumb
+        paths={[
+          {
+            label: 'Root',
+            url: '/',
+          },
+          {
+            label: 'Parent',
+            url: '/parent',
+          },
+          {
+            label: 'Child',
+            url: '/parent/child',
+          },
+        ]}
+      />,
+    );
+
+    expect(screen.getAllByText('>').length).toEqual(2);
+    expect(screen.getByText('Root')).toHaveAttribute('href', '/');
+    expect(screen.getByText('Parent')).toHaveAttribute('href', '/parent');
+    expect(screen.getByText('Child')).toHaveAttribute('href', '/parent/child');
+  });
+});
---
+++ src/app/component/breadcrumb.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+interface Path {
+  label: string;
+  url: string;
+}
+
+interface Props {
+  paths: Path[];
+}
+
+export default function Breadcrumb(props: Partial<Props>) {
+  return (
+    <div className="flex text-gray-500 text-sm leading-5">
+      {props.paths?.map((item, index) => (
+        <Fragment key={index}>
+          <Link href={item.url}>{item.label}</Link>
+          {index < props.paths!.length - 1 && (
+            <span className="mx-1">&gt;</span>
+          )}
+        </Fragment>
+      ))}
+    </div>
+  );
+}
