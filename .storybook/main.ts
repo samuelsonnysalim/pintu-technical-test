0ffdcd770e47187d802c7ea8955d70dc768b2bc3
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-27 20:04:42 +0700

    Add @storybook/addon-postcss addon and resolve alias for @pintu/technical-test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,20 @@
+import path from 'path';
 import type { StorybookConfig } from '@storybook/nextjs';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -12,6 +22,13 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pintu/technical-test': path.resolve(__dirname, '../src/'),
+    };
+    return config;
   },
 };
 export default config;
