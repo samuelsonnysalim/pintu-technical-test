5e7c19f59fd9921605a249489668cfa063e87e90
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-29 01:03:13 +0700

    Implement mobile layout on PriceTable component

--- src/app/component/__tests__/price-table.test.tsx
+++ src/app/component/__tests__/price-table.test.tsx
@@ -1,9 +1,11 @@
 import nock from 'nock';
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ClientProvider from '@pintu/technical-test/app/client-provider';
 import PriceTable from '@pintu/technical-test/app/component/price-table';
 import WalletService from '@pintu/technical-test/service/wallet-service';
 import TradeService from '@pintu/technical-test/service/trade-service';
+import { resizeScreenSize } from '@pintu/technical-test/setup/util';
 
 jest.mock('@pintu/technical-test/service/wallet-service');
 jest.mock('@pintu/technical-test/service/trade-service');
@@ -168,36 +170,136 @@ describe('PriceTable', () => {
     );
 
     await waitFor(() => {
-      expect(screen.getByText('CRYPTO')).toBeInTheDocument();
+      expect(screen.getAllByText('CRYPTO')[1]).toBeInTheDocument();
       expect(screen.getByText('HARGA')).toBeInTheDocument();
-      expect(screen.getByText('24 JAM')).toBeInTheDocument();
-      expect(screen.getByText('1 MGG')).toBeInTheDocument();
-      expect(screen.getByText('1 BLN')).toBeInTheDocument();
-      expect(screen.getByText('1 THN')).toBeInTheDocument();
+      expect(screen.getAllByText('24 JAM')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 MGG')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 BLN')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 THN')[1]).toBeInTheDocument();
+
+      expect(container.querySelectorAll('.injected-svg')[2]).toHaveAttribute(
+        'data-src',
+        'http://localhost/static.pintu.co.id/assets/images/logo/circle_BTC.svg',
+      );
+      expect(screen.getAllByText('Bitcoin')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('BTC')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 395.954.932')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('0.83%')[1]).toBeInTheDocument();
+      expect(screen.getByText('1.28%')).toBeInTheDocument();
+      expect(screen.getByText('4.22%')).toBeInTheDocument();
+      expect(screen.getByText('8.67%')).toBeInTheDocument();
+
+      expect(container.querySelectorAll('.injected-svg')[3]).toHaveAttribute(
+        'data-src',
+        'http://localhost/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
+      );
+      expect(screen.getAllByText('Ethereum')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('ETH')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 27.124.120')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1.55%')[1]).toBeInTheDocument();
+      expect(screen.getByText('0.48%')).toBeInTheDocument();
+      expect(screen.getByText('1.68%')).toBeInTheDocument();
+      expect(screen.getByText('4.85%')).toBeInTheDocument();
+    });
+  });
+
+  it('should render price changes table on mobile layout', async () => {
+    resizeScreenSize(400);
+    const { container } = render(
+      <ClientProvider>
+        <PriceTable />
+      </ClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getAllByText('CRYPTO')[0]).toBeInTheDocument();
+      expect(screen.getByTitle('Select Date Range')).toBeInTheDocument();
 
       expect(container.querySelectorAll('.injected-svg')[0]).toHaveAttribute(
         'data-src',
         'http://localhost/static.pintu.co.id/assets/images/logo/circle_BTC.svg',
       );
-      expect(screen.getByText('Bitcoin')).toBeInTheDocument();
-      expect(screen.getByText('BTC')).toBeInTheDocument();
-      expect(screen.getByText('Rp 395.954.932')).toBeInTheDocument();
-      expect(screen.getByText('0.83%')).toBeInTheDocument();
-      expect(screen.getByText('1.28%')).toBeInTheDocument();
-      expect(screen.getByText('4.22%')).toBeInTheDocument();
-      expect(screen.getByText('8.67%')).toBeInTheDocument();
+      expect(screen.getAllByText('Bitcoin')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('BTC')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 395.954.932')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('0.83%')[0]).toBeInTheDocument();
 
       expect(container.querySelectorAll('.injected-svg')[1]).toHaveAttribute(
         'data-src',
         'http://localhost/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
       );
-      expect(screen.getByText('Ethereum')).toBeInTheDocument();
-      expect(screen.getByText('ETH')).toBeInTheDocument();
-      expect(screen.getByText('Rp 27.124.120')).toBeInTheDocument();
-      expect(screen.getByText('1.55%')).toBeInTheDocument();
-      expect(screen.getByText('0.48%')).toBeInTheDocument();
-      expect(screen.getByText('1.68%')).toBeInTheDocument();
-      expect(screen.getByText('4.85%')).toBeInTheDocument();
+      expect(screen.getAllByText('Ethereum')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('ETH')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 27.124.120')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('1.55%')[0]).toBeInTheDocument();
+    });
+  });
+
+  it('should be able to select date range to "24 JAM" and render percentage based on it on mobile layout', async () => {
+    const user = userEvent.setup({ delay: null });
+    resizeScreenSize(400);
+    render(
+      <ClientProvider>
+        <PriceTable />
+      </ClientProvider>,
+    );
+
+    await user.selectOptions(screen.getByTitle('Select Date Range'), 'day');
+
+    await waitFor(() => {
+      expect(screen.getAllByText('0.83%')).toHaveLength(2);
+      expect(screen.getAllByText('1.55%')).toHaveLength(2);
+    });
+  });
+
+  it('should be able to select date range to "1 MGG" and render percentage based on it on mobile layout', async () => {
+    const user = userEvent.setup({ delay: null });
+    resizeScreenSize(400);
+    render(
+      <ClientProvider>
+        <PriceTable />
+      </ClientProvider>,
+    );
+
+    await user.selectOptions(screen.getByTitle('Select Date Range'), 'week');
+
+    await waitFor(() => {
+      expect(screen.getAllByText('1.28%')).toHaveLength(2);
+      expect(screen.getAllByText('0.48%')).toHaveLength(2);
+    });
+  });
+
+  it('should be able to select date range to "1 BLN" and render percentage based on it on mobile layout', async () => {
+    const user = userEvent.setup({ delay: null });
+    resizeScreenSize(400);
+    render(
+      <ClientProvider>
+        <PriceTable />
+      </ClientProvider>,
+    );
+
+    await user.selectOptions(screen.getByTitle('Select Date Range'), 'month');
+
+    await waitFor(() => {
+      expect(screen.getAllByText('4.22%')).toHaveLength(2);
+      expect(screen.getAllByText('1.68%')).toHaveLength(2);
+    });
+  });
+
+  it('should be able to select date range to "1 THN" and render percentage based on it on mobile layout', async () => {
+    const user = userEvent.setup({ delay: null });
+    resizeScreenSize(400);
+    render(
+      <ClientProvider>
+        <PriceTable />
+      </ClientProvider>,
+    );
+
+    await user.selectOptions(screen.getByTitle('Select Date Range'), 'year');
+
+    await waitFor(() => {
+      expect(screen.getAllByText('8.67%')).toHaveLength(2);
+      expect(screen.getAllByText('4.85%')).toHaveLength(2);
     });
   });
 
@@ -209,47 +311,47 @@ describe('PriceTable', () => {
     );
 
     await waitFor(() => {
-      expect(screen.getByText('CRYPTO')).toBeInTheDocument();
+      expect(screen.getAllByText('CRYPTO')[1]).toBeInTheDocument();
       expect(screen.getByText('HARGA')).toBeInTheDocument();
-      expect(screen.getByText('24 JAM')).toBeInTheDocument();
-      expect(screen.getByText('1 MGG')).toBeInTheDocument();
-      expect(screen.getByText('1 BLN')).toBeInTheDocument();
-      expect(screen.getByText('1 THN')).toBeInTheDocument();
+      expect(screen.getAllByText('24 JAM')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 MGG')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 BLN')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 THN')[1]).toBeInTheDocument();
 
-      expect(container.querySelectorAll('.injected-svg')[0]).toHaveAttribute(
+      expect(container.querySelectorAll('.injected-svg')[2]).toHaveAttribute(
         'data-src',
         'http://localhost/static.pintu.co.id/assets/images/logo/circle_BTC.svg',
       );
-      expect(screen.getByText('Bitcoin')).toBeInTheDocument();
-      expect(screen.getByText('BTC')).toBeInTheDocument();
-      expect(screen.getByText('Rp 395.954.932')).toBeInTheDocument();
-      expect(screen.getByText('0.83%')).toBeInTheDocument();
+      expect(screen.getAllByText('Bitcoin')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('BTC')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 395.954.932')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('0.83%')[1]).toBeInTheDocument();
       expect(screen.getByText('1.28%')).toBeInTheDocument();
       expect(screen.getByText('4.22%')).toBeInTheDocument();
       expect(screen.getByText('8.67%')).toBeInTheDocument();
 
-      expect(container.querySelectorAll('.injected-svg')[1]).toHaveAttribute(
+      expect(container.querySelectorAll('.injected-svg')[3]).toHaveAttribute(
         'data-src',
         'http://localhost/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
       );
-      expect(screen.getByText('Ethereum')).toBeInTheDocument();
-      expect(screen.getByText('ETH')).toBeInTheDocument();
-      expect(screen.getByText('Rp 27.124.120')).toBeInTheDocument();
-      expect(screen.getByText('1.55%')).toBeInTheDocument();
+      expect(screen.getAllByText('Ethereum')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('ETH')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 27.124.120')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1.55%')[1]).toBeInTheDocument();
       expect(screen.getByText('0.48%')).toBeInTheDocument();
       expect(screen.getByText('1.68%')).toBeInTheDocument();
       expect(screen.getByText('4.85%')).toBeInTheDocument();
     });
 
     await waitFor(() => {
-      expect(screen.getByText('Rp 397.412.558')).toBeInTheDocument();
-      expect(screen.getByText('0.99%')).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 397.412.558')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('0.99%')[1]).toBeInTheDocument();
       expect(screen.getByText('0.95%')).toBeInTheDocument();
       expect(screen.getByText('3.71%')).toBeInTheDocument();
       expect(screen.getByText('8.33%')).toBeInTheDocument();
 
-      expect(screen.getByText('Rp 27.335.136')).toBeInTheDocument();
-      expect(screen.getByText('1.67%')).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 27.335.136')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1.67%')[1]).toBeInTheDocument();
       expect(screen.getByText('0.85%')).toBeInTheDocument();
       expect(screen.getByText('0.68%')).toBeInTheDocument();
       expect(screen.getByText('4.11%')).toBeInTheDocument();
@@ -264,21 +366,21 @@ describe('PriceTable', () => {
     );
 
     await waitFor(() => {
-      expect(screen.getByText('CRYPTO')).toBeInTheDocument();
+      expect(screen.getAllByText('CRYPTO')[1]).toBeInTheDocument();
       expect(screen.getByText('HARGA')).toBeInTheDocument();
-      expect(screen.getByText('24 JAM')).toBeInTheDocument();
-      expect(screen.getByText('1 MGG')).toBeInTheDocument();
-      expect(screen.getByText('1 BLN')).toBeInTheDocument();
-      expect(screen.getByText('1 THN')).toBeInTheDocument();
+      expect(screen.getAllByText('24 JAM')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 MGG')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 BLN')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1 THN')[1]).toBeInTheDocument();
 
-      expect(container.querySelectorAll('.injected-svg')[0]).toHaveAttribute(
+      expect(container.querySelectorAll('.injected-svg')[1]).toHaveAttribute(
         'data-src',
         'http://localhost/static.pintu.co.id/assets/images/logo/circle_ETH.svg',
       );
-      expect(screen.getByText('Ethereum')).toBeInTheDocument();
-      expect(screen.getByText('ETH')).toBeInTheDocument();
-      expect(screen.getByText('Rp 27.124.120')).toBeInTheDocument();
-      expect(screen.getByText('1.55%')).toBeInTheDocument();
+      expect(screen.getAllByText('Ethereum')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('ETH')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('Rp 27.124.120')[1]).toBeInTheDocument();
+      expect(screen.getAllByText('1.55%')[1]).toBeInTheDocument();
       expect(screen.getByText('0.48%')).toBeInTheDocument();
       expect(screen.getByText('1.68%')).toBeInTheDocument();
       expect(screen.getByText('4.85%')).toBeInTheDocument();
--- src/app/component/price-table.tsx
+++ src/app/component/price-table.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 import classNames from 'classnames';
 import { useQuery } from '@tanstack/react-query';
@@ -16,6 +17,8 @@ interface Props {
   currencies: string[];
 }
 
+type DateRange = 'day' | 'week' | 'month' | 'year';
+
 export default function PriceTable(props: Partial<Props>) {
   const { data: supportedCurrenciesData } =
     useQuery<SupportedCurrenciesResponse>({
@@ -27,6 +30,7 @@ export default function PriceTable(props: Partial<Props>) {
     queryFn: () => TradeService.listPriceChanges(),
     refetchInterval: 1000,
   });
+  const [dateRange, setDateRange] = useState<DateRange>('day');
 
   const filteredSupportedCurrencies = supportedCurrenciesData?.payload.filter(
     ({ currencySymbol }) =>
@@ -35,7 +39,7 @@ export default function PriceTable(props: Partial<Props>) {
   );
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col -mx-4 xl:mx-0">
       {isLoading && <Loading clasName="mt-10 !w-12 !h-12 self-center" />}
       {(error as Error) && (
         <Message className="mt-8" title="Telah Terjadi Error" type="error">
@@ -43,94 +47,159 @@ export default function PriceTable(props: Partial<Props>) {
         </Message>
       )}
       {supportedCurrenciesData && data && (
-        <table className="table-auto w-full border-separate">
-          <thead>
-            <tr>
-              <th className="border border-r-0 border-gray-200 rounded-tl-lg p-5 pl-[75px] text-left text-gray-500 leading-[22px] font-semibold">
+        <>
+          {/* MOBILE */}
+          <div className="xl:hidden">
+            <div className="flex items-center p-4 border-t border-b border-gray-200">
+              <span className="flex-none grow text-xs font-semibold">
                 CRYPTO
-              </th>
-              <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
-                HARGA
-              </th>
-              <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
-                24 JAM
-              </th>
-              <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
-                1 MGG
-              </th>
-              <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
-                1 BLN
-              </th>
-              <th className="border border-l-0 border-gray-200 rounded-tr-lg p-5 text-left text-gray-500 leading-[22px] font-semibold">
-                1 THN
-              </th>
-            </tr>
-          </thead>
-          <tbody>
+              </span>
+              <select
+                className="border border-gray-200 p-1 text-xs font-semibold rounded-lg"
+                title="Select Date Range"
+                value={dateRange}
+                onChange={(e) =>
+                  setDateRange(e.target.selectedOptions[0]?.value as DateRange)
+                }
+              >
+                <option value="day">24 JAM</option>
+                <option value="week">1 MGG</option>
+                <option value="month">1 BLN</option>
+                <option value="year">1 THN</option>
+              </select>
+            </div>
             {filteredSupportedCurrencies?.map((item, index) => {
               const price = data?.payload.find(
                 ({ pair }) =>
                   pair === `${item.currencySymbol.toLowerCase()}/idr`,
               );
               return (
-                <tr key={index}>
-                  <td
-                    className={classNames(
-                      'border-b border-l border-gray-200 p-5',
-                      {
-                        'rounded-bl-lg':
-                          index === filteredSupportedCurrencies.length - 1,
-                      },
-                    )}
-                  >
-                    <div className="flex">
-                      <ReactSVG
-                        className="w-8 h-8 mr-6"
-                        beforeInjection={(svg) => {
-                          svg.setAttribute(
-                            'style',
-                            `width: 2rem; height: 2rem; color: ${item.color}`,
-                          );
-                        }}
-                        src={item.logo}
-                        title={`${item.name} Logo`}
-                      />
-                      <span className="flex-none grow font-semibold leading-8">
-                        {item.name}
-                      </span>
-                      <span className="w-12 text-gray-500 text-right leading-8">
+                <div
+                  key={index}
+                  className="flex items-center p-4 border-b border-gray-200"
+                >
+                  <ReactSVG
+                    className="w-8 h-8 mr-6"
+                    beforeInjection={(svg) => {
+                      svg.setAttribute(
+                        'style',
+                        `width: 2rem; height: 2rem; color: ${item.color}`,
+                      );
+                    }}
+                    src={item.logo}
+                    title={`${item.name} Logo`}
+                  />
+                  <div className="flex grow">
+                    <div className="flex flex-col w-1/2">
+                      <span className="font-semibold">{item.name}</span>
+                      <span className="text-sm text-gray-500">
                         {item.currencySymbol}
                       </span>
                     </div>
-                  </td>
-                  <td className="border-b border-gray-200 p-5 font-semibold">
-                    <Currency value={price?.latestPrice} />
-                  </td>
-                  <td className="border-b border-gray-200 p-5 font-semibold">
-                    <Percentage value={price?.day} />
-                  </td>
-                  <td className="border-b border-gray-200 p-5 font-semibold">
-                    <Percentage value={price?.week} />
-                  </td>
-                  <td className="border-b border-gray-200 p-5 font-semibold">
-                    <Percentage value={price?.month} />
-                  </td>
-                  <td
-                    className={classNames(
-                      'border-b border-r border-gray-200 p-5 font-semibold',
-                      {
-                        'rounded-br-lg':
-                          index === filteredSupportedCurrencies.length - 1,
-                      },
-                    )}
-                  >
-                    <Percentage value={price?.year} />
-                  </td>
-                </tr>
+                    <div className="flex flex-col w-1/2">
+                      <div className="text-right font-semibold">
+                        <Currency value={price?.latestPrice} />
+                      </div>
+                      <div className="flex justify-end text-sm">
+                        <Percentage value={price ? price[dateRange] : 0} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               );
             })}
-          </tbody>
-        </table>
+          </div>
+
+          {/* DESKTOP */}
+          <table className="hidden xl:table table-auto w-full border-separate">
+            <thead>
+              <tr>
+                <th className="border border-r-0 border-gray-200 rounded-tl-lg p-5 pl-[75px] text-left text-gray-500 leading-[22px] font-semibold">
+                  CRYPTO
+                </th>
+                <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
+                  HARGA
+                </th>
+                <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
+                  24 JAM
+                </th>
+                <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
+                  1 MGG
+                </th>
+                <th className="border-y border-gray-200 p-5 text-left text-gray-500 leading-[22px] font-semibold">
+                  1 BLN
+                </th>
+                <th className="border border-l-0 border-gray-200 rounded-tr-lg p-5 text-left text-gray-500 leading-[22px] font-semibold">
+                  1 THN
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredSupportedCurrencies?.map((item, index) => {
+                const price = data?.payload.find(
+                  ({ pair }) =>
+                    pair === `${item.currencySymbol.toLowerCase()}/idr`,
+                );
+                return (
+                  <tr key={index}>
+                    <td
+                      className={classNames(
+                        'border-b border-l border-gray-200 p-5',
+                        {
+                          'rounded-bl-lg':
+                            index === filteredSupportedCurrencies.length - 1,
+                        },
+                      )}
+                    >
+                      <div className="flex">
+                        <ReactSVG
+                          className="w-8 h-8 mr-6"
+                          beforeInjection={(svg) => {
+                            svg.setAttribute(
+                              'style',
+                              `width: 2rem; height: 2rem; color: ${item.color}`,
+                            );
+                          }}
+                          src={item.logo}
+                          title={`${item.name} Logo`}
+                        />
+                        <span className="flex-none grow font-semibold leading-8">
+                          {item.name}
+                        </span>
+                        <span className="w-12 text-gray-500 text-right leading-8">
+                          {item.currencySymbol}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="border-b border-gray-200 p-5 font-semibold">
+                      <Currency value={price?.latestPrice} />
+                    </td>
+                    <td className="border-b border-gray-200 p-5 font-semibold">
+                      <Percentage value={price?.day} />
+                    </td>
+                    <td className="border-b border-gray-200 p-5 font-semibold">
+                      <Percentage value={price?.week} />
+                    </td>
+                    <td className="border-b border-gray-200 p-5 font-semibold">
+                      <Percentage value={price?.month} />
+                    </td>
+                    <td
+                      className={classNames(
+                        'border-b border-r border-gray-200 p-5 font-semibold',
+                        {
+                          'rounded-br-lg':
+                            index === filteredSupportedCurrencies.length - 1,
+                        },
+                      )}
+                    >
+                      <Percentage value={price?.year} />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   );
