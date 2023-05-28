2d925e0a2c77758daf789c50ebaeca52b49261e5
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-28 12:13:31 +0700

    Add Cache-Control on price-changes api

--- src/app/api/trade/price-changes/route.ts
+++ src/app/api/trade/price-changes/route.ts
@@ -2,12 +2,16 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const res = await fetch('https://api.pintu.co.id/v2/trade/price-changes', {
-    cache: 'no-cache',
+    cache: 'no-store',
     headers: {
       'Content-Type': 'application/json',
     },
   });
   const data = await res.json();
 
-  return NextResponse.json(data);
+  return NextResponse.json(data, {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+    },
+  });
 }
